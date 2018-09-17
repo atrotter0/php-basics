60b08460535cfc0ddc09c2ea41eaee2b39e42f92
Abel Trotter <a_trotter0@hotmail.com>
2018-09-17 16:51:28 -0700

    Add basic order form example

---
+++ order-form.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Basics</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="css/styles.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="js/scripts.js"></script>
+</head>
+<body>
+  <div class="container">
+    <div class="container">
+    <h1>Order Form:</h1>
+    <div>
+      <form action="order.php">
+        <div class="form-group">
+          <label for="name">Full Name:</label>
+          <input type="text" name="name" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="address">Street Address:</label>
+          <input type="text" name="address" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="apt">Apt/Unit:</label>
+          <input type="text" name="apt" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="city">City:</label>
+          <input type="text" name="city" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="state">State:</label>
+          <input type="text" name="state" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Place Order</button>
+        <a href="index.php"><button type="button" class="btn btn-primary">Back</button></a>
+      </form>
+    </div>
+  </div>
+  </div>
+</body>
+</html>
---
+++ order.php
@@ -0,0 +1,37 @@
+<?php
+    $name = $_GET["name"];
+    $street_address = $_GET["address"];
+    $apt = $_GET["apt"];
+    $city = $_GET["city"];
+    $state = $_GET["state"];
+    $zip = $_GET["zip"];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Basics</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="css/styles.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="js/scripts.js"></script>
+</head>
+<body>
+  <div class="container">
+    <h1>Sample Order</h1>
+    <h2>Order Details:</h2>
+    <p>Thank you for placing your order. Your order details are listed below:</p>
+    <p>
+      Name: <?php echo $name ?><br/>
+      Street Address: <?php echo $street_address ?><br/>
+      Apt/Unit: <?php echo $apt ?><br/>
+      City: <?php echo $city ?><br/>
+      State: <?php echo $state ?>
+    </p>
+    <hr/>
+    <a href="index.php">Home</a>
+  </div>
+</body>
+</html>
